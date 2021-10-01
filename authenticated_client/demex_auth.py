6c8b91fdf676ffea7e51583673162a3e97e55946
c1im4cu5 <38188027+c1im4cu5@users.noreply.github.com>
2021-09-30 22:56:10 -0600

    Update demex_auth.py
    Added separate functions for Tradehub authenticated client, authenticated Demex client and public client

--- authenticated_client/demex_auth.py
+++ authenticated_client/demex_auth.py
@@ -1,9 +1,24 @@
 from tradehub.demex_client import DemexClient
+from tradehub.authenticated_client import AuthenticatedClient
+from tradehub.public_account_client import PublicClient
+from tradehub.wallet import Wallet
 
-def main():
-    mnemonic='ENTER YOUR MNEMONIC HERE FOR CONNECTION TO DEMEX EXCHANGE'
+mnemonic='ENTER YOUR MNEMONIC HERE FOR CONNECTION TO TRADEHUB'
+
+def dem_client():
     dem_client = DemexClient(mnemonic= mnemonic,
                 network='mainnet',
                 trusted_ips=None,
                 trusted_uris=['http://175.41.151.35:5001', 'http://54.255.5.46:5001'])
     return dem_client
+
+def auth_client():
+    ac = AuthenticatedClient(network='mainnet',
+                    wallet=Wallet(mnemonic=mnemonic, network='mainnet'),
+                    trusted_ips=None,
+                    trusted_uris=["http://54.255.5.46:5001", "http://175.41.151.35:5001"])
+    return ac
+
+def p_client():
+    pc = PublicClient(network='mainnet', trusted_uris=["http://54.255.5.46:5001", "http://175.41.151.35:5001"])
+    return pc
