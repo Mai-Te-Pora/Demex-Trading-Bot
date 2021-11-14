92cb42fc91e0d19e5b4ec2b416758f2915aec3f5
c1im4cu5 <38188027+c1im4cu5@users.noreply.github.com>
2021-11-14 14:18:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ async def on_connect():
                                                 #f"books.{'swth_busd1'}",
                                                 #f"candlesticks.{'swth_usdc1'}.{15}",
                                                 #f"balances.{address}",
-                                                f"orders.{'ENTER PUB ADDRESS}"])
+                                                f"orders.{address}"])
 
 #Receiving feed from websocket
 async def on_receive(records: dict):
