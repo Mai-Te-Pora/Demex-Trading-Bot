ad3b93331cffac89a2a284c8e4d29975b5c5fa8d
c1im4cu5 <38188027+c1im4cu5@users.noreply.github.com>
2021-11-13 20:31:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from tradehub.websocket_client import DemexWebsocket
 import asyncio
+import json
 
 import sys, os, logging
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from data_processing import ReceivingRecords
 from data_processing import CleaningRecords
 from data_processing import SavingRecords
+from data_processing import CancelOrders
 from data_processing import *
 from strategies import Treway
 from strategies import Grid
+from strategies import Grid2
 from authenticated_client import demex_auth
 
 
-address = demex_auth.rtn_address()
 balances = []
 orders = []
+hld = []
 market_stats = []
 swth_usdc = []
 swth_busd = []
@@ -37,8 +40,8 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
-#On Error - Websocket loses connection (for whatever reason)
-#All orders from Grid bot will be canceled
+address = demex_auth.rtn_address()
+
 async def on_error():
     print("Websocket connection has been terminated")
     try:
@@ -54,17 +57,17 @@ async def on_error():
 async def on_connect():
     #candlestick granularity - allowed values: 1, 5, 15, 30, 60, 360, 1440
     return await demex.subscribe("Subscription", [
-                                                f"market_stats.{'market_stats'}", 
-                                                #f"books.{'wbtc1_usdc1'}", 
-                                                #f"books.{'eth1_wbtc1'}", 
-                                                #f"books.{'cel_eth'}", 
-                                                #f"books.{'cel1_usdc1'}", 
-                                                #f"books.{'eth1_usdc1'}", 
-                                                #f"books.{'swth_usdc1'}", 
-                                                #f"books.{'swth_eth1'}", 
-                                                #f"books.{'swth_busd1'}", 
-                                                #f"candlesticks.{'swth_usdc1'}.{15}", 
-                                                #f"balances.{address}", 
+                                                f"market_stats.{'market_stats'}",
+                                                #f"books.{'wbtc1_usdc1'}",
+                                                #f"books.{'eth1_wbtc1'}",
+                                                #f"books.{'cel_eth'}",
+                                                #f"books.{'cel1_usdc1'}",
+                                                #f"books.{'eth1_usdc1'}",
+                                                #f"books.{'swth_usdc1'}",
+                                                #f"books.{'swth_eth1'}",
+                                                #f"books.{'swth_busd1'}",
+                                                #f"candlesticks.{'swth_usdc1'}.{15}",
+                                                #f"balances.{'ENTER YOUR WALLET ADDRESS HERE'}",
                                                 f"orders.{address}"])
 
 #Receiving feed from websocket
@@ -82,11 +85,9 @@ async def on_receive(records: dict):
         #Wallet Orders
         #Check if orders in record
         if 'orders.' in records['channel']:
-            #Monitoring incoming orders against pre-built user defined grid bot orders
-            Grid.GridBot().monitor_limit_orders(records)
+            Grid.monitor_limit_orders(records)
             print("Active Orders Updated")
             print("Websocket will stay active without printing status until next order update.")
-
 
         #Market Statistics
         if 'market_stats' in records['channel']:
@@ -105,14 +106,6 @@ async def on_receive(records: dict):
             CleaningRecords.cleaning_orderbooks(swth_usdc)
             #Send to function for saving file
             SavingRecords.save_swth_usdc_orderbook(swth_usdc)
-        #Check if swth_busd books are in the "channel"
-        if 'books.swth_busd1' in records['channel']:
-            #Send data to Receiving records, return back list of dicts; which are extended with updates
-            swth_busd.extend(ReceivingRecords.swth_busd_book(records))
-            #Clean the records
-            CleaningRecords.cleaning_orderbooks(swth_busd)
-            #Send to function for saving file
-            SavingRecords.save_swth_busd_orderbook(swth_busd)
         #Check if swth_eth books are in the "channel"
         if 'books.swth_eth1' in records['channel']:
             #Send data to Receiving records, return back list of dicts; which are extended with updates
@@ -129,22 +122,6 @@ async def on_receive(records: dict):
             CleaningRecords.cleaning_orderbooks(eth_usdc)
             #Send to function for saving file
             SavingRecords.save_eth_usdc_orderbook(eth_usdc)
-        #Check if swth_eth books are in the "channel"
-        if 'books.cel_eth' in records['channel']:
-            #Send data to Receiving records, return back list of dicts; which are extended with updates
-            cel_eth.extend(ReceivingRecords.cel_eth_book(records))
-            #Clean the records
-            CleaningRecords.cleaning_orderbooks(cel_eth)
-            #Send to function for saving file
-            SavingRecords.save_cel_eth_orderbook(cel_eth)
-        #Check if swth_eth books are in the "channel"
-        if 'books.cel1_usdc1' in records['channel']:
-            #Send data to Receiving records, return back list of dicts; which are extended with updates
-            cel_usdc.extend(ReceivingRecords.cel_usdc_book(records))
-            #Clean the records
-            CleaningRecords.cleaning_orderbooks(cel_usdc)
-            #Send to function for saving file
-            SavingRecords.save_cel_usdc_orderbook(cel_usdc)
         #Check if eth_wbtc books are in the "channel"
         if 'books.eth1_wbtc1' in records['channel']:
             #Send data to Receiving records, return back list of dicts; which are extended with updates
@@ -164,28 +141,41 @@ async def on_receive(records: dict):
 
 async def bot_task():
     while True:
-        Treway.TrewayBot().main()
-        print("No trades to perform. Sleeping for two minutes.")
+        Treway.analyze_eth("1.25", "0.0125")
+        root.info("No trades to perform. Sleeping for two minutes.")
         await asyncio.sleep(120)
 
 async def main():
-    #Gathering user orders
-    orders = Grid.GridBot().create_limit_orders()
-    SavingRecords.save_active_orders(orders)
+    orders = []
+
+    #Print any active orders (FOR EXPERIENCED USERS)
+    Grid2.print_active_orders()
+
+    loading_user = input("Please ignore this request unless you are an experience user. The websocket will, at times, lose connection. Existing orders are stored in a json file at '/data_processing/logs/active_orders.json'. This file is a list of dicts. Users can freely adjust the parameters of this file, load them or just reload exisiting acitive orders. If you possessed active orders on file, they would have printed out above this message. If so, you can restart the bot to continue monitoring those orders.  Would you like to monitor old orders or generate new orders (old/new): ")
+    if loading_user == 'new':
+        Grid.print_markets()
+        Grid.clean_potential_orders()
+        Grid.clean_active_orders()
+        loop = 0
+        Grid.question_hub()
+        orders = Grid.get_active_orders()
+        orders = Grid.create_limit_orders(orders)
+        SavingRecords.save_active_orders(orders)
+        print("Orders Saved...proceeding with monitor status")
+    elif loading_user == 'old':
+        pass
+
+    print("**************  Websocket will stay active with no activity in terminal  *********************")
 
     #Create Websocket asyncio task
     socket = asyncio.create_task(demex.connect(on_receive, on_connect))
-
     #Create Treway Bot task via bot_task function
-    #Blocking out Treway bot creation from usage - Please unblock if you'd like to utilize the strategy
-    #bot = asyncio.create_task(bot_task())
 
     #Gather and run functions concurrently
     asyncio.gather(
-                    asyncio.get_event_loop().run_until_complete(await socket),
+                    asyncio.get_event_loop().run_until_complete(await socket)
                     #asyncio.get_event_loop().run_until_complete(await bot)
                     )
-
 
 if __name__ == '__main__':
     demex: DemexWebsocket = DemexWebsocket('wss://ws.dem.exchange/ws')
